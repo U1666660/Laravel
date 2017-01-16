769f70d84c5fa53bb75089f17d9aa24ba44958dd
Advanced Web <u1666660@unimail.hud.ac.uk>
2017-01-16 08:11:26 +0000

    Deleting resources with CRUD

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,7 +35,8 @@
                   <td>{{ $post->title }}</td>
                   <td>{{ substr($post->body, 0, 50) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
                   <td>{{ date('M j, Y H:i', strtotime($post->created_at)) }}</td>
-                  <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a> <a href="route('posts.edit', $post->id)" class="btn btn-default btn-sm">Edit</a></td>
+                  <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default btn-sm">View</a>
+                    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default btn-sm">Edit</a></td>
 
 
                 </tr>
