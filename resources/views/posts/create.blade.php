e4c3aac8c0424c485a4a9ffe424a47d6cc380272
Advanced Web <u1666660@unimail.hud.ac.uk>
2017-01-21 19:36:47 +0000

    Added a WYSIWYG Editor via tinymce

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,64 +3,74 @@
 @section('title', '| Create New Post')
 
 
-@section('stylesheet')
+  @section('stylesheets')
 
   {!! Html::style('css/parsley.css') !!}
-  {!! Html::style('css/select2.min.css') !!}
+	{!! Html::style('css/select2.min.css') !!}
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 
-@endsection
-
-
-@section('content')
-
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <h1>Create New Post</h1>
-          <hr>
-          {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
-              {{ Form::label('title', 'Title:') }}
-              {{ Form::text('title', null,array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
-
-              {{ Form::label('slug', 'Slug:') }}
-              {{ Form::text('slug', null, array('class' => 'form-control', 'required' => '', 'minlength' => '5', 'maxlength' => '255')) }}
-
-              {{ Form::label('category_id', 'Category:') }}
-              <select class="form-control" name="category_id">
-                @foreach($categories as $category)
-                  <option value='{{ $category->id }}'>{{ $category->name }}</option>
-                @endforeach
-              </select>
-
-              {{ Form::label('tags', 'Tags:') }}
-              <select class="form-control select2-multi" name="tags[]" multiple="multiple">
-
-                @foreach($tags as $tag)
-                  <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
-                @endforeach
-
-              </select>
+	<script>
+		tinymce.init({
+			selector: 'textarea',
+			plugins: 'link code',
+			menubar: false
+		});
+	</script>
 
 
-              {{ Form::label('body', 'Post Body:', array('style' => 'margin-top:20px')) }}
-              {{ Form::textarea('body', null, array('class' => 'form-control','required'=> '')) }}
+  @endsection
 
-              {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top:20px;')) }}
 
-          {!! Form::close() !!}
+  @section('content')
 
-        </div>
+  	<div class="row">
+  		<div class="col-md-8 col-md-offset-2">
+  			<h1>Create New Post</h1>
+  			<hr>
+  			{!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '', 'files' => true)) !!}
+  				{{ Form::label('title', 'Title:') }}
+  				{{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
-    </div>
+  				{{ Form::label('slug', 'Slug:') }}
+  				{{ Form::text('slug', null, array('class' => 'form-control', 'required' => '', 'minlength' => '5', 'maxlength' => '255') ) }}
 
-@endsection
+  				{{ Form::label('category_id', 'Category:') }}
+  				<select class="form-control" name="category_id">
+  					@foreach($categories as $category)
+  						<option value='{{ $category->id }}'>{{ $category->name }}</option>
+  					@endforeach
+
+  				</select>
+
+
+  				{{ Form::label('tags', 'Tags:') }}
+  				<select class="form-control select2-multi" name="tags[]" multiple="multiple">
+  					@foreach($tags as $tag)
+  						<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+  					@endforeach
+
+  				</select>
+
+  				{{ Form::label('featured_img', 'Upload a Featured Image') }}
+  				{{ Form::file('featured_img') }}
+
+  				{{ Form::label('body', "Post Body:") }}
+  				{{ Form::textarea('body', null, array('class' => 'form-control')) }}
+
+  				{{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
+  			{!! Form::close() !!}
+  		</div>
+  	</div>
+
+  @endsection
 
 @section('scripts')
 
-  {!! Html::style('js/parsley.min.js') !!}
-  {!! Html::style('js/select2.full.min.js') !!}
+	{!! Html::script('js/parsley.min.js') !!}
+	{!! Html::script('js/select2.min.js') !!}
 
-  <script type="text/javascript">
-  		$('.select2-multi').select2();
-  	</script>
+	<script type="text/javascript">
+		$('.select2-multi').select2();
+	</script>
 
 @endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,35 +2,43 @@
 
 @section('title', '| Edit Blog Post')
 
-@section('stylesheet')
+  @section('stylesheets')
 
-    {!! Html::style('css/select2.min.css') !!}
+  	{!! Html::style('css/select2.min.css') !!}
 
-@endsection
+  	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 
+  	<script>
+  		tinymce.init({
+  			selector: 'textarea',
+  			plugins: 'link code',
+  			menubar: false
+  		});
+  	</script>
+
+  @endsection
 
 @section('content')
 
   <div class="row">
-    {!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PUT']) !!}
-    <div class="col-md-8">
-      {{ Form::label('title', 'Title:') }}
-      {{ Form::text('title', null, ['class' => 'form-control input-lg']) }}
+		{!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PUT']) !!}
+		<div class="col-md-8">
+			{{ Form::label('title', 'Title:') }}
+			{{ Form::text('title', null, ["class" => 'form-control input-lg']) }}
 
-      {{ Form::label('slug', 'Slug:', ['style' => 'margin-top:20px']) }}
-      {{ Form::text('slug', null, ['class' => 'form-control']) }}
+			{{ Form::label('slug', 'Slug:', ['class' => 'form-spacing-top']) }}
+			{{ Form::text('slug', null, ['class' => 'form-control']) }}
 
-      {{ Form::label('category_id', 'Category:', ['style' => 'margin-top:20px']) }}
-      {{ Form::select('category_id', $categories, null, ['class' => 'form-control']) }}
+			{{ Form::label('category_id', "Category:", ['class' => 'form-spacing-top']) }}
+			{{ Form::select('category_id', $categories, null, ['class' => 'form-control']) }}
 
-      {{ Form::label('tags', 'Tags:', ['style' => 'margin-top:20px']) }}
-      {{ Form::select('tag[]', $tags, null, ['class' => 'form-control select2-multi', 'multiple' => 'multiple']) }}
+			{{ Form::label('tags', 'Tags:', ['class' => 'form-spacing-top']) }}
+			{{ Form::select('tags[]', $tags, null, ['class' => 'form-control select2-multi', 'multiple' => 'multiple']) }}
 
-
-      {{ Form::label('body', 'Body:', ['style' => 'margin-top:20px']) }}
-      {{ Form::textarea('body', null, ['class' => 'form-control']) }}
-
+			{{ Form::label('body', "Body:", ['class' => 'form-spacing-top']) }}
+			{{ Form::textarea('body', null, ['class' => 'form-control']) }}
     </div>
+
 
       <div class="col-md-4">
         <div class="well">
@@ -44,36 +52,29 @@
               <dd>{{ date('M j, Y H:i', strtotime($post->updated_at)) }}</dd>
             </dl>
             <hr>
+				        <div class="row">
+					          <div class="col-sm-6">
+						        {!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+					          </div>
+					      <div class="col-sm-6">
+						       {{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
+					      </div>
+				       </div>
 
-            <div class="row">
-              <div class="col-sm-6">
-                {!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
-
-              </div>
-              <div class="col-sm-6">
-                {{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
-
-              </div>
-            </div>
-
-            </div>
-
-        </div>
-        {!! Form::close() !!}
-      </div>  <!-- end of .row(form) -->
+			        </div>
+	 </div>
+		                {!! Form::close() !!}
+  </div> <!-- end of .row (form) -->
 
 @endsection
 
 @section('scripts')
 
-  {!! Html::style('js/select2.full.min.js') !!}
+	{!! Html::script('js/select2.min.js') !!}
 
-  <script type="text/javascript">
-
-      $('.select2-multi').select2();
-  		$('.select2-multi').select2().val({!! json_encode($post->tags()->getRelatedIds()) !!}).trigger('change');
-
-
-  </script>
+	<script type="text/javascript">
+		$('.select2-multi').select2();
+		$('.select2-multi').select2().val({!! json_encode($post->tags()->getRelatedIds()) !!}).trigger('change');
+	</script>
 
 @endsection
