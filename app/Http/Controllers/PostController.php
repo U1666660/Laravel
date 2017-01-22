66fa11b2c2ff32a171432ea0b51b0ce7f52c4677
Advanced Web <u1666660@unimail.hud.ac.uk>
2017-01-22 15:42:25 +0000

    Added Posts Authentication

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,10 @@ class PostController extends Controller
 {
 
 
-
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
     /**
      * Display a listing of the resource.
      *
