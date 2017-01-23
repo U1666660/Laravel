42cc8691890d72a404b8ffda5970f55808938553
Advanced Web <u1666660@unimail.hud.ac.uk>
2017-01-23 00:17:26 +0000

    Modified About page interface

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,8 +10,7 @@
       <p class="lead">
         <pre style="text-align: center;"><strong>
 
-  Hello! Yunqingblog is my first website built via laravel.
-  You could access via http://138.68.47.216 or pengyunqing.com
+  Hello! Yunqingblog(pengyunqing.com) is my first website built via laravel.
 
   In Yunqingblog you could register an account for login, write your own post and slug, create category or tag, comment under post.
   If you have any suggestions or questions, please go to Contact Page send message to me or send email to pengyunqing730@gmail.com
